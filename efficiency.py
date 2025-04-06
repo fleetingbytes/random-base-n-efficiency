c436edab6057af522fdacfb2ecfad0a64baa3096
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-04-06 13:36:34 +0200

    feat: cli

--- efficiency.py
+++ efficiency.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+from sys import argv
 from math import ceil
 from dataclasses import dataclass, field, InitVar
+from typing import ClassVar
+
 
 @dataclass
 class Efficiency:
@@ -12,6 +15,7 @@ class Efficiency:
     wasted_bits_per_digit: float = field(init=False)
     base: InitVar[int]
     digits: InitVar[int]
+    formatting: ClassVar[dict[str, str]]
 
     def __post_init__(self, base: int, digits: int):
         biggest_needed = base ** digits - 1
@@ -23,8 +27,14 @@ class Efficiency:
         probability_of_waste = 1 - (base ** digits / 2 ** self.bits)
         self.wasted_bits_per_digit = probability_of_waste * self.bits / digits
 
-for digits in range(1, 1025):
-    e = Efficiency(7, digits)
-    if e.efficiency < 0.994:
+for digits in range(1, 2049):
+    base = int(argv[1])
+    e = Efficiency(base, digits)
+    if e.efficiency < 0.999:
         continue
     print(f"{digits:>4} {e.efficiency:.4f} {e.bits:>4} {e.bytes:>3} {e.residual_bits} {e.wasted_bits_per_digit:.6f} wasted bits/digit")
+    if e.efficiency >= 1.0:
+        break
+
+def print_header():
+    pass
